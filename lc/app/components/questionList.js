20489208ccae9ffa3929481db2388e211f96d37e
Siyuan Liu <liusy182@gmail.com>
2019-09-04 21:35:35 -0700

    update html styler

--- lc/app/components/questionList.js
+++ lc/app/components/questionList.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import { StyleSheet, Text, View, FlatList } from 'react-native';
+import HTML from 'react-native-render-html';
 
-export default class QuestionList extends Component {
+
+class QuestionList extends Component {
     render() {
         return (
             <View style={styles.container}>
                 <View>
                     <FlatList
-                        data={[{ key: 'a' }, { key: 'b' }, { key: 'c' }, { key: 'd' }]}
-                        renderItem={({ item }) => <Text>{item.key}</Text>}
+                        data={this.props.questions}
+                        renderItem={({item}) => (
+                            <View>
+                                <Text>{item.title}</Text>
+                                <HTML html={item.content} />
+                            </View>
+                        )}
                     />
                 </View>
             </View>
         );
     }
 }
+
+const mapStateToProps = ({questions}) => ({
+    questions,
+})
+
+const mapDispatchToProps = dispatch => ({
+    
+})
+
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(QuestionList)
+
 
 const styles = StyleSheet.create({
     container: {
--- lc/app/reducers/questions.js
+++ lc/app/reducers/questions.js
@@ -1,20 +1,27 @@
-const questions = (state = [], action) => {
+const defaultState = [{
+    key: '1',
+    id: 1,
+    title: 'Two Sum',
+    content: `<p>Given an array of integers, return <strong>indices</strong> of the two numbers such that they add up to a specific target.</p>
+<p>You may assume that each input would have <strong><em>exactly</em></strong> one solution, and you may not use the <em>same</em> element twice.</p>
+<p><strong>Example:</strong></p>
+<pre>
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[<strong>0</strong>] + nums[<strong>1</strong>] = 2 + 7 = 9,
+return [<strong>0</strong>, <strong>1</strong>].
+</pre>
+`,
+    tags: [
+        'Array',
+        'Hash Table',
+    ]
+}]
+
+const questions = (state = defaultState, action) => {
     switch (action.type) {
-        case 'ADD_TODO':
-            return [
-                ...state,
-                {
-                    id: action.id,
-                    text: action.text,
-                    completed: false
-                }
-            ]
-        case 'TOGGLE_TODO':
-            return state.map(todo =>
-                (todo.id === action.id)
-                    ? { ...todo, completed: !todo.completed }
-                    : todo
-            )
+        case 'GET_QUESTIONS':
+            return ;
         default:
             return state
     }
