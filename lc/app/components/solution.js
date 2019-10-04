3ef62e4a0611cf6229c5ad6f20ce67a012879a19
Siyuan Liu <liusy182@gmail.com>
2019-10-03 22:24:57 -0700

    iframe

--- lc/app/components/solution.js
+++ lc/app/components/solution.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import { StyleSheet, ScrollView, View } from 'react-native';
+import { StyleSheet, ScrollView, View, Text } from 'react-native';
 import HTML from 'react-native-render-html';
 
 
 class Solution extends Component {
+    renderers = {
+        a: (htmlAttribs, children, convertedCSSStyles, passProps) => {
+            const { key, data } = passProps;
+            return (
+                <Text key={key}>
+                    {children || data}
+                </Text>
+            );
+        },
+        iframe: (htmlAttribs, children, convertedCSSStyles, passProps) => {
+            const { key, data } = passProps;
+            return (
+                <Text key={key}>
+                    {'iframe'}
+                </Text>
+            );
+        }
+    }
+
     render() {
         const { solution } = this.props;
         return (
             <View style={styles.container}>
                 <ScrollView>
-                    <HTML html={solution.content} />
+                    <HTML html={solution.content} renderers={this.renderers} solution={solution} />
                 </ScrollView>
             </View>
         );
@@ -41,6 +60,8 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'flex-start',
         alignItems: 'stretch',
+        paddingHorizontal: 8,
+        paddingVertical: 8,
     },
     title: {
         fontSize: 24,
